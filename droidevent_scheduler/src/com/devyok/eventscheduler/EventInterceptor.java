da0a1b8ffcd981d145bf0b978eea1d943015e8fd
devyok <dengwei_686@126.com>
2017-12-21 10:37:26 +0800

    remove

--- droidevent_scheduler/src/com/devyok/eventscheduler/EventInterceptor.java
+++
@@ -1,8 +0,0 @@
-package com.devyok.eventscheduler;
-/**
- * 事件拦截 
- * @author wei.deng
- */
-public interface EventInterceptor {
-	public boolean onIntercept(Event e);
-}
--- droidevent_scheduler/src/com/devyok/eventscheduler/Schedulable.java
+++
@@ -1,4 +0,0 @@
-package com.devyok.eventscheduler;
-
-public interface Schedulable {
-}
